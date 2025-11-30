f7e432b5a174144f7c97df920d15ef4f8a34fc1a
Omega Mputu <kutekemeni93@gmail.com>
2025-11-30 14:59:33 +0100

    Notifications table created and user create issue fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +28,21 @@ class AppServiceProvider extends ServiceProvider
         // Le Super-Admin passe avant tous les checks
         Gate::before(function ($user, $ability) {
             return $user->hasRole('Owner') ? true : null;
+        });
+
+        ResetPassword::toMailUsing(function ($notifiable, $token) {
+        $url = url(route('invite.accept', [
+            'token' => $token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        return (new MailMessage)
+            ->subject('Invitation')
+            ->greeting('Bonjour '.$notifiable->name)
+            ->line("Vous avez été invité à utiliser l’application de gestion des arbitres de la FECOFA.")
+            ->action('Définir mon mot de passe', $url)
+            ->line('Ce lien expirera dans '.config('auth.passwords.invites.expire').' minutes.')
+            ->line("Si vous n’êtes pas à l’origine de cette invitation, ignorez cet email.");
         });
 
         Gate::policy(User::class, \App\Policies\UserPolicy::class);
