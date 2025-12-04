488629f0b81365ab5aa1561a9aa51bbf80b07a67
Omega Mputu <kutekemeni93@gmail.com>
2025-12-04 15:55:07 +0100

    Instruction pagination updated

--- resources/views/livewire/instructors/edit.blade.php
+++ resources/views/livewire/instructors/edit.blade.php
@@ -138,7 +138,7 @@ new class extends Component {
                 'after_or_equal:issue_date',
             ],
 
-            'start_year' => ['nullable', 'integer', 'min:1950', 'max:' . date('Y')],
+            'start_year' => ['nullable', 'integer', 'min:1940', 'max:' . date('Y')],
             'referee_category_id' => ['required', 'exists:referee_categories,id'],
             'referee_role_id' => ['required', 'exists:referee_roles,id'],
             'instructor_role_id' => ['required', 'exists:instructor_roles,id'],
